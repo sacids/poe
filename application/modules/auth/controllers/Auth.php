1c2ec29c7fa4bcc224089ec2d6a4854fa8557f72
Renfrid Ngolongolo <renfridfrancis@gmail.com>
2020-04-24 07:31:51 +0300

    Change login redirection

--- application/modules/auth/controllers/Auth.php
+++ application/modules/auth/controllers/Auth.php
@@ -22,7 +22,7 @@ class Auth extends CI_Controller
         if (!$this->ion_auth->logged_in()) {
             redirect('auth/login', 'refresh');
         } else {
-            redirect('Admin', 'refresh');
+            redirect('admin', 'refresh');
         }
     }
 
