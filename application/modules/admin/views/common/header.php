d63933f395d35ad519b9cec1ec0ef8377d6ddd40
Renfrid Ngolongolo <renfridfrancis@gmail.com>
2020-04-24 08:39:07 +0300

    remove unused content

--- application/modules/admin/views/common/header.php
+++ application/modules/admin/views/common/header.php
@@ -1,4 +1,12 @@
 <div class="header border-bottom d-flex flex-row-reverse align-middle" >
-  <div class="border-left p-2">User</div>
-  <div class="border-left p-2">Messages</div>
+  <div class="border-left p-2"><a href="#" class="navbar-nav-link dropdown-toggle" data-toggle="dropdown">
+          <span><?php echo get_current_user_name() ?></span>
+      </a>
+
+      <div class="dropdown-menu dropdown-menu-right">
+          <a href="<?= site_url('auth/logout') ?>" class="dropdown-item" style="font-size: 12px;"><i class="icon-switch2"></i>
+              Logout</a>
+      </div>
+  </div>
+  <!--<div class="border-left p-2">Messages</div>-->
 </div>
